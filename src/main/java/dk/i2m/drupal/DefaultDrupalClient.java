8bf22fb8d42063c01fc01e8fe5a777f4a45a00ea
Raymond Wanyoike <rwa@i2m.dk>
2012-08-04 23:34:31 +0300

    Add DrupalHttpClient and DefaultDrupalClient

---
+++ src/main/java/dk/i2m/drupal/DefaultDrupalClient.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2012 Interactive Media Management
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.i2m.drupal;
+
+import dk.i2m.drupal.core.AbstractDrupalClient;
+import java.net.URI;
+
+/**
+ * Default implementation of {@link DrupalClient} pre-configured for most 
+ * common use scenarios.
+ * 
+ * @author Raymond Wanyoike <rwa at i2m.dk>
+ */
+public class DefaultDrupalClient extends AbstractDrupalClient {
+
+    /**
+     * Creates a new Drupal client from a hostname and endpoint.
+     * 
+     * @param hostname  the druapl server URL
+     * @param endPoint  the services endpoint
+     */
+    public DefaultDrupalClient(URI hostname, String endPoint) {
+        super(hostname, endPoint);
+    }
+}

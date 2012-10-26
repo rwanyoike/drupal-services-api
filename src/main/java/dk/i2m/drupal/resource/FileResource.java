47c0b04fc374cccde50a078af18309cf56d1892d
Raymond Wanyoike <rwa@i2m.dk>
2012-10-26 03:35:47 +0300

    Code refactoring. Correct createRaw() return value

--- src/main/java/dk/i2m/drupal/resource/FileResource.java
+++ src/main/java/dk/i2m/drupal/resource/FileResource.java
@@ -21,30 +21,19 @@ import com.google.gson.reflect.TypeToken;
 import dk.i2m.drupal.core.AbstractResourceCRUD;
 import dk.i2m.drupal.core.DrupalClient;
 import dk.i2m.drupal.message.FileMessage;
+import dk.i2m.drupal.util.EntityUtils;
 import dk.i2m.drupal.util.URLBuilder;
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Type;
 import java.util.List;
-import org.apache.http.Consts;
+import java.util.Map;
 import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.tika.config.TikaConfig;
-import org.apache.tika.io.IOUtils;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.mime.MediaType;
-import org.apache.tika.mime.MimeType;
 import org.apache.tika.mime.MimeTypeException;
-import org.apache.tika.mime.MimeTypes;
 
 /**
  * @author Raymond Wanyoike <rwa at i2m.dk>
@@ -55,7 +44,7 @@ public class FileResource extends AbstractResourceCRUD {
         super(dc);
     }
 
-    public FileMessage createRaw(File file, String fileName) throws
+    public List<FileMessage> createRaw(Map<File, String> files) throws
             HttpResponseException, MimeTypeException, IOException {
         URLBuilder builder = new URLBuilder(getDc().getHostname());
         builder.add(getDc().getEndpoint());
@@ -63,16 +52,14 @@ public class FileResource extends AbstractResourceCRUD {
         builder.add("create_raw");
 
         HttpPost method = new HttpPost(builder.toURI());
-        method.setEntity(getMultipartEntity(file, fileName));
+        method.setEntity(EntityUtils.getMultipartEntity(files));
 
         ResponseHandler<String> handler = new BasicResponseHandler();
         String response = getDc().getHttpClient().execute(method, handler);
 
-        Type typeOfT = new TypeToken<List<FileMessage>>() {
-        }.getType();
-        List<FileMessage> fromJson = new Gson().fromJson(response, typeOfT);
-
-        return fromJson.get(0);
+        return new Gson().fromJson(response,
+                new TypeToken<List<FileMessage>>() {
+                }.getType());
     }
 
     public FileMessage retrieve(Long id) throws HttpResponseException,
@@ -103,25 +90,6 @@ public class FileResource extends AbstractResourceCRUD {
         String response = getDc().getHttpClient().execute(method, handler);
 
         return Boolean.valueOf(response);
-    }
-
-    private MultipartEntity getMultipartEntity(File file, String fileName)
-            throws MimeTypeException, IOException {
-        TikaConfig tikaConfig = TikaConfig.getDefaultConfig();
-        MimeTypes mimeTypes = tikaConfig.getMimeRepository();
-        byte[] buf = IOUtils.toByteArray(new FileInputStream(file));
-        InputStream in = new ByteArrayInputStream(buf);
-        MediaType mediaType = mimeTypes.detect(in, new Metadata());
-        MimeType mimeType = mimeTypes.forName(mediaType.toString());
-        MultipartEntity multipartEntity = new MultipartEntity();
-
-        int x = 0;
-
-        FileBody fb = new FileBody(file, fileName + mimeType.getExtension(),
-                mimeType.getName(), Consts.UTF_8.name());
-        multipartEntity.addPart("files[" + x + "]", fb);
-
-        return multipartEntity;
     }
 
     @Override
